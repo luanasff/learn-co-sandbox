fe45d1481e7fd07a27a3cce94881cfc125712f66
Luana Faria Lane <luanasfff@gmail.com>
2020-08-06 01:07:37 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,20 @@
+puts "Hello there"
+puts "Luana"
+puts "and Trey"
+
+print 123
+print 456
+
+
+
+ grocery_items = {:apples => 10, :pears => 4, :peaches => 2, :plums => 13}
+
+ 
+grocery_items[:rambutans]
+
+grocery_items = {:apples => 10, :pears => 4, :peaches => 2, :plums => 13}
+
+if grocery_items[:rambutan]
+  puts "Rambutan present!"
+else
+  puts "No rambutan."
